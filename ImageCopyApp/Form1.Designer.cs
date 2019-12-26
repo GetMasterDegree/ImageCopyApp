967ce8075d3d16dbcc06c77e86550bfc46736eab
CJDaniel96 <cjtsai96@gmail.com>
2019-12-26 20:40:13 +0800

    ImageCopyApp

---
+++ ImageCopyApp/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ImageCopyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Filefo = new System.Windows.Forms.Label();
+            this.Filefotext = new System.Windows.Forms.TextBox();
+            this.Filetotext = new System.Windows.Forms.TextBox();
+            this.Fileto = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Endexit = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Filefo
+            // 
+            this.Filefo.AutoSize = true;
+            this.Filefo.Location = new System.Drawing.Point(185, 180);
+            this.Filefo.Name = "Filefo";
+            this.Filefo.Size = new System.Drawing.Size(41, 15);
+            this.Filefo.TabIndex = 0;
+            this.Filefo.Text = "label1";
+            // 
+            // Filefotext
+            // 
+            this.Filefotext.Location = new System.Drawing.Point(337, 170);
+            this.Filefotext.Name = "Filefotext";
+            this.Filefotext.Size = new System.Drawing.Size(224, 25);
+            this.Filefotext.TabIndex = 1;
+            this.Filefotext.TextChanged += new System.EventHandler(this.Filefotext_TextChanged);
+            // 
+            // Filetotext
+            // 
+            this.Filetotext.Location = new System.Drawing.Point(337, 233);
+            this.Filetotext.Name = "Filetotext";
+            this.Filetotext.Size = new System.Drawing.Size(224, 25);
+            this.Filetotext.TabIndex = 2;
+            // 
+            // Fileto
+            // 
+            this.Fileto.AutoSize = true;
+            this.Fileto.Location = new System.Drawing.Point(185, 243);
+            this.Fileto.Name = "Fileto";
+            this.Fileto.Size = new System.Drawing.Size(41, 15);
+            this.Fileto.TabIndex = 3;
+            this.Fileto.Text = "label2";
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(188, 326);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 23);
+            this.Submit.TabIndex = 4;
+            this.Submit.Text = "button1";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(337, 326);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 5;
+            this.Reset.Text = "button2";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Endexit
+            // 
+            this.Endexit.Location = new System.Drawing.Point(486, 326);
+            this.Endexit.Name = "Endexit";
+            this.Endexit.Size = new System.Drawing.Size(75, 23);
+            this.Endexit.TabIndex = 6;
+            this.Endexit.Text = "button3";
+            this.Endexit.UseVisualStyleBackColor = true;
+            this.Endexit.Click += new System.EventHandler(this.Endexit_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(185, 67);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(41, 15);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.Endexit);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.Fileto);
+            this.Controls.Add(this.Filetotext);
+            this.Controls.Add(this.Filefotext);
+            this.Controls.Add(this.Filefo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Filefo;
+        private System.Windows.Forms.TextBox Filefotext;
+        private System.Windows.Forms.TextBox Filetotext;
+        private System.Windows.Forms.Label Fileto;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Endexit;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ ImageCopyApp/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageCopyApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(700, 420);
+
+            lblTitle.Location = new Point(180, 40);
+            lblTitle.AutoSize = true;
+            lblTitle.Text = "資料自動轉存";
+            lblTitle.Font = new Font("新細明體", 32, FontStyle.Regular);
+
+            Filefo.Location = new Point(180, 185);
+            Filefo.AutoSize = true;
+            Filefo.Text = "檔案位置:";
+            Filefo.Font = new Font("新細明體", 14, FontStyle.Regular);
+
+            Filefotext.Location = new Point(320, 180);
+            Filefotext.AutoCompleteSource = AutoCompleteSource.FileSystemDirectories;
+            Filefotext.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            Filefotext.Font = new Font("新細明體", 10, FontStyle.Regular);
+            Filefotext.Text = "";
+
+            Fileto.Location = new Point(180, 235);
+            Fileto.AutoSize = true;
+            Fileto.Text = "存放位置:";
+            Fileto.Font = new Font("新細明體", 14, FontStyle.Regular);
+
+            Filetotext.Location = new Point(320, 230);
+            Filetotext.AutoCompleteSource = AutoCompleteSource.FileSystemDirectories;
+            Filetotext.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            Filetotext.Font = new Font("新細明體", 10, FontStyle.Regular);
+            Filetotext.Text = "";
+
+            Submit.Location = new Point(180, 300);
+            Submit.AutoSize = true;
+            Submit.Text = "確定";
+            Submit.Visible = true;
+            Submit.Enabled = true;
+
+            Reset.Location = new Point(320, 300);
+            Reset.AutoSize = true;
+            Reset.Text = "重置";
+            Reset.Visible = true;
+            Reset.Enabled = true;
+
+            Endexit.Location = new Point(460, 300);
+            Endexit.AutoSize = true;
+            Endexit.Text = "退出";
+            Endexit.Visible = true;
+            Endexit.Enabled = true;
+        }
+
+        private void Filefotext_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Endexit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("請問是否退出程式?", "資料自動轉存", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dirfor;
+            DirectoryInfo dirto;
+
+            Filefotext.Text = "c:\\Users\\Daniel\\Downloads\\abc";
+            Filetotext.Text = "d:\\";
+
+            try
+            {
+                dirfor = new DirectoryInfo(Filefotext.Text);
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("請確實填寫路徑!!!");
+                return;
+            }
+
+            if (!dirfor.Exists)
+            {
+                MessageBox.Show("請確實填寫路徑!!!");
+                return;
+            }
+
+            try
+            {
+                dirto = new DirectoryInfo(Filetotext.Text);
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("目的路徑不存在!!!");
+                if (MessageBox.Show("請問是否在桌面創建資料夾?", "資料自動轉存", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.OK)
+                {
+                    dirfor = new DirectoryInfo(Filefotext.Text);
+                    dirto = new DirectoryInfo("C:\\Users\\Daniel\\Desktop");
+                    DataCopyApp(dirfor, dirto);
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("請確實填寫路徑!!!");
+                    return;
+                }
+            }
+
+            if(Filefotext.Text != null && Filetotext.Text != null)
+            {
+                dirfor = new DirectoryInfo(Filefotext.Text);
+                dirto = new DirectoryInfo(Filetotext.Text);
+                DataCopyApp(dirfor, dirto);
+            }
+        }
+
+        //資料自動轉存
+        private void DataCopyApp(DirectoryInfo dirfordata, DirectoryInfo dirtodata)
+        {
+            FileInfo[] filefordata = dirfordata.GetFiles();
+            DateTime[] filefortime = new DateTime[filefordata.Length];
+            //將資料時間存入陣列
+            for(int i = 0; i < filefordata.Length; i++)
+            {
+                filefortime[i] = filefordata[i].CreationTime.Date;
+            }
+            //將資料依時間排序
+            filefortime = DateTimeBySort(filefortime);
+            DirectoryInfo[] dirtime = new DirectoryInfo[filefortime.Length];
+
+            for (int i = 0; i < filefortime.Length; i++)
+            {
+                dirtime[i] = Createdir(dirtodata, filefortime[i]);
+                for(int j = 0; j < filefordata.Length; j++)
+                {
+
+                        filefordata[j].CopyTo(dirtime[i].FullName + "\\" + filefordata[j]);
+                }
+            }
+        }
+
+        //資料依時間由近到遠排序
+        private DateTime[] DateTimeBySort(DateTime[] filefortime)
+        {
+            DateTime temp;
+
+            for(int i = 0; i < filefortime.Length; i++)
+            {
+                for(int j = 0; j < filefortime.Length; j++)
+                {
+                    if(DateTime.Compare(filefortime[i], filefortime[j]) < 0)
+                    {
+                        temp = filefortime[i];
+                        filefortime[i] = filefortime[j];
+                        filefortime[j] = temp;
+                    }
+                }
+            }
+
+            return filefortime;
+        }
+
+        //創建新資料夾
+        private DirectoryInfo Createdir(DirectoryInfo dirtodata, DateTime filetime)
+        {
+            string filetimepath = "\\" + filetime.ToString("yyyyMMdd");
+            string dirtopath = dirtodata.ToString();
+            string dirpath = dirtopath + filetimepath;
+            DirectoryInfo newdir = Directory.CreateDirectory(dirpath);
+            
+            return newdir;
+        }
+    }
+}
